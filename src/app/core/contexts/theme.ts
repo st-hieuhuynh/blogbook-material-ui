e347177f5cb9b95d60887861f9627f8dccd28cb0
st-hieuhuynh <hieu.huynh@supremetech.vn>
2022-03-08 13:13:07 +0700

    fix convention

--- src/app/core/contexts/theme.ts
+++ src/app/core/contexts/theme.ts
@@ -1,35 +1,35 @@
-import { createMuiTheme } from "@material-ui/core";
+import { createMuiTheme } from '@material-ui/core';
 
 const theme = createMuiTheme({
   breakpoints: {
     values: {
-      "xs" : 480,
-      "sm" : 770,
-      "md": 960,
-      "lg": 1024,
-      "xl": 1270,
+      'xs' : 480,
+      'sm' : 770,
+      'md': 960,
+      'lg': 1024,
+      'xl': 1270,
     },
   },
   palette: {
     primary: {
-      main: "#0078ff",
-      dark: "#005de7"
+      main: '#0078ff',
+      dark: '#005de7'
     },
     error: {
-      main: "#dc3545"
+      main: '#dc3545'
     },
     success: {
-      main: "#198754"
+      main: '#198754'
     },
     warning: {
-      main: "#f9bb00"
+      main: '#f9bb00'
     }
   },
   typography: {
     htmlFontSize: 10,
     fontSize: 16,
     fontFamily: [
-      "Inter", "sans-serif"
+      'Inter', 'sans-serif'
     ].join(','),
   }
-})
+});
